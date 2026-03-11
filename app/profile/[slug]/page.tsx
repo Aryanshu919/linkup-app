ee1f81c7571ca4a402f1c10aa7d8c08740825fd9
Aryanshu919 <aryanshu919@gmail.com>
2026-03-11 23:09:01 +0530

    fixed the slug inside the profile folder

--- app/profile/[slug]/page.tsx
+++ app/profile/[slug]/page.tsx
@@ -3,11 +3,11 @@ import { prisma } from "@/lib/prisma";
 export default async function Profile({
  params
 }: {
- params:{ slug:string }
+  params: Promise<{ slug: string }>;
 }) {
 
  const profile = await prisma.profile.findUnique({
-  where:{ slug:params.slug },
+  where:{ userId:(await params).slug },
   include:{
    user:{
     include:{ links:true }
@@ -16,17 +16,17 @@ export default async function Profile({
  });
 
  if(!profile) return <div>Not found</div>;
+ console.log(profile);
 
  return (
 
-  <div className="min-h-screen flex flex-col items-center justify-center">
+  <div className="min-h-screen bg-blue-300 flex flex-col items-center justify-center">
 
    <h1 className="text-3xl font-bold">
     {profile.user.name || "Profile"}
    </h1>
 
    <div className="mt-6 space-y-3">
-
     {profile.user.links.map(link=>(
      <a
       key={link.id}
